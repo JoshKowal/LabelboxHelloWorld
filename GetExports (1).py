1ceba2c722b9d7274ca25bfb4c01acebdce461b7
Josh Kowal <endgamekowal@gmail.com>
2021-01-10 21:06:11 -0600

    Added project export files

---
+++ GetExports (1).py
@@ -0,0 +1,13 @@
+import TestExports
+from TestExports import project_to_json
+
+PROJECT_NAME = 'African Animals'
+
+#Uncomment if running on Windows:
+#project_to_json(PROJECT_NAME, 'Windows')
+
+#Uncomment if running on Mac:
+project_to_json(PROJECT_NAME, 'Mac')
+
+#Uncomment if running on Linux:
+#project_to_json(PROJECT_NAME, 'Linux')
